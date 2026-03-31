192a86d777642dc79fc3d6e8e9f70c5f431037f6
Grant-XCENA <grant.mackey@xcena.com>
2026-03-30 17:00:36 -0700

    MLC was updated to 3.12 and Intel doesn't keep old versions around on their site. Need to find a fix going forward or this is going to break every new release
    Signed-off-by: Grant-XCENA <grant.mackey@xcena.com>

--- src/container-runtime/hardware/MLC/get_mlc.sh
+++ src/container-runtime/hardware/MLC/get_mlc.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-wget https://downloadmirror.intel.com/793041/mlc_v3.11.tgz
-mkdir -p mlc_v3.11
-tar xf mlc_v3.11.tgz -C mlc_v3.11
-rm mlc_v3.11.tgz
+#TODO: figure out a better way to do this. Every time intel updates MLC this will break.
+#      Need some sort of automation/email/something so that it can at least be updated,
+#      but moving away from MLC to something else may be better long term.
+#      ALSO, intel isn't checking their certs right now either so I've had to toss in
+#      the no check flag, which also isn't my favorite.
+#wget https://downloadmirror.intel.com/793041/mlc_v3.11.tgz
+wget --no-check-certificate https://downloadmirror.intel.com/866182/mlc_v3.12.tgz
+mkdir -p mlc_v3.12
+tar xf mlc_v3.12.tgz -C mlc_v3.12
+rm mlc_v3.12.tgz
